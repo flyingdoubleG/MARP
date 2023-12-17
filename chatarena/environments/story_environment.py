96fbaca7ea39f18527227d1ff72f376b83ec4ce3
Gonghan Xu <gonghan@Bens-MacBook-Pro.local>
2023-12-17 11:57:31 -0500

    minor fix

--- chatarena/environments/story_environment.py
+++ chatarena/environments/story_environment.py
@@ -123,7 +123,7 @@ class Story(Environment):
 
     def _parse_env_manager_output(self, text: str) -> str:
         try:
-            summary = text.split('### Summary: ')[1]
+            summary = text.split('### Summary:')[1]
             return summary
         except IndexError:
             print(f'WARNING env manager output format error')
